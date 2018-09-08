c8f0aeaf87480678c7de41000d532f38c5b4131d
Task Nagashige <task.19.xx@gmail.com>
2018-09-09 01:54:19 +0900

    fix indent.

--- a-star.lua
+++ a-star.lua
@@ -179,10 +179,10 @@ function path ( start, goal, nodes, ignore_cache, valid_node_func )
 		return cachedPaths [ start ] [ goal ]
 	end
 
-    local resPath = a_star ( start, goal, nodes, valid_node_func )
-    if not cachedPaths [ start ] [ goal ] and not ignore_cache then
-        cachedPaths [ start ] [ goal ] = resPath
-    end
+        local resPath = a_star ( start, goal, nodes, valid_node_func )
+        if not cachedPaths [ start ] [ goal ] and not ignore_cache then
+                cachedPaths [ start ] [ goal ] = resPath
+        end
 
 	return resPath
 end
